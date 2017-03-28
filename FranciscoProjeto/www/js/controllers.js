87b7b78db2031cde0bf0b3d3ec426789c8ffbf09
marceloroot <marcelo.lima.gomes.23@gmail.com>
2017-03-28 09:58:55 -0300

    marcelo 28/03/2017/09:58

--- FranciscoProjeto/www/js/controllers.js
+++ FranciscoProjeto/www/js/controllers.js
@@ -56,91 +56,35 @@ angular.module('starter.controllers', [])
 .controller('PlaylistCtrl', function ($scope, $stateParams) {
 })
 
-.controller('browseCtrl', function ($scope) {
-    var Latitude = undefined;
-    var Longitude = undefined;
+.controller('browseCtrl', function ($scope, $rootScope, $ionicLoading) {
+    
+   $rootScope.Locais = [
+   { Lat: 28.03932552928991, Long: 38.168157445898785 },
+   
 
-    // Get geo coordinates
+    ];
+   var onSuccess = function (position) {
 
-    function getMapLocation() {
-
-        navigator.geolocation.getCurrentPosition
-        (onMapSuccess, onMapError, { enableHighAccuracy: true });
-    }
-
-    // Success callback for get geo coordinates
-
-    var onMapSuccess = function (position) {
-
-        Latitude = position.coords.latitude;
-        Longitude = position.coords.longitude;
-
-        getMap(Latitude, Longitude);
-
-    }
-
-    // Get map by using coordinates
-
-    function getMap(latitude, longitude) {
-
-        var mapOptions = {
-            center: new google.maps.LatLng(0, 0),
-            zoom: 1,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-
-        map = new google.maps.Map
-        (document.getElementById("map"), mapOptions);
-
-
-        var latLong = new google.maps.LatLng(latitude, longitude);
-
-        var marker = new google.maps.Marker({
-            position: latLong
-        });
-
-        marker.setMap(map);
-        map.setZoom(15);
-        map.setCenter(marker.getPosition());
-    }
-
-    // Success callback for watching your changing position
-
-    var onMapWatchSuccess = function (position) {
-
-        var updatedLatitude = position.coords.latitude;
-        var updatedLongitude = position.coords.longitude;
-
-        if (updatedLatitude != Latitude && updatedLongitude != Longitude) {
-
-            Latitude = updatedLatitude;
-            Longitude = updatedLongitude;
-
-            getMap(updatedLatitude, updatedLongitude);
-        }
-    }
-
-    // Error callback
-
-    function onMapError(error) {
-        console.log('code: ' + error.code + '\n' +
-            'message: ' + error.message + '\n');
-    }
-
-    // Watch your changing position
-
-    function watchMapPosition() {
-
-        return navigator.geolocation.watchPosition
-        (onMapWatchSuccess, onMapError, { enableHighAccuracy: true });
-    }
-
-
-
+       var posic = { Lat: position.coords.latitude, Long: position.coords.longitude}
+       $rootScope.Locais.push(posic);
+       $ionicLoading.hide();
+   };
+   function onError(error) {
+       alert('code: ' + error.code + '\n' +
+             'message: ' + error.message + '\n');
+   };
+   $scope.chamaValor = function()
+   {
+       navigator.geolocation.getCurrentPosition(onSuccess, onError);
+       $ionicLoading.show({ template: 'Loading...' });
+    
+   }
+  
+  
 })
 
 
-.controller('SearchCtrl', function ($scope, $ionicLoading) {
+.controller('SearchCtrl', function ($scope, $ionicLoading, $rootScope) {
 
     $ionicLoading.show({ template: 'Loading...' });
 
@@ -192,15 +136,15 @@ angular.module('starter.controllers', [])
         return Angle
     }
    
-
-    var Locais = [
+   
+   /* var Locais = [
     { Lat: 28.03932552928991, Long: 38.168157445898785, heade: 279 },
     { Lat: 43.465119, Long: -50.522375, heade: 60.90, },
     { Lat: 43.465193, Long: -90.522386, heade: 90.90 },
     { Lat: 43.465240, Long: -90.522389, heade: 90.90 },
     { Lat: 60.465195, Long: 90.522388, heade: 90.90 }
 
-    ];
+    ];*/
     var sons = [];
 
     // onSuccess Callback
@@ -213,7 +157,7 @@ angular.module('starter.controllers', [])
     // $scope.resposta[Locais.length.valueOf()] = null;  
     var onSuccess = function (position) {
         
-        Locais.forEach(function (item, index) {
+        $rootScope.Locais.forEach(function (item, index) {
             $scope.resultado.push(calcDistancia(position.coords.latitude, position.coords.longitude, item.Lat, item.Long));
             $scope.posicao.push(calcAngulobearing(position.coords.latitude, position.coords.longitude, item.Lat, item.Long, position.coords.heading))
 
@@ -246,6 +190,10 @@ angular.module('starter.controllers', [])
         else if ((posicao > 160) && (posicao < 190)) {
             retorno = retorno + "e a " + posicao.toFixed(2) + " graus sua costa";
         }
+        else if (posicao == 0) {
+
+            retorno = retorno;
+        }
         else {
             retorno = retorno + "e a " + posicao.toFixed(2) + " graus sua direita";
 
@@ -270,40 +218,43 @@ angular.module('starter.controllers', [])
             }
             else if ((item =>1) && (item < 2)) {
 
-                $scope.retorna.push($scope.posicao[index],item)
+                $scope.retorna.push(buscarposicao($scope.posicao[index], item));
                 sons.push('src/objetoummentrodireita.mp3');
 
 
             }
             else if ((item =>2) && (item < 3)) {
-                $scope.retorna.push($scope.posicao[index], item)
+                $scope.retorna.push(buscarposicao($scope.posicao[index], item));
                 sons.push('src/objetodoismentrodireita.mp3');
             }
             else if ((item =>3) && (item < 4)) {
-                $scope.retorna.push($scope.posicao[index], item)
+                $scope.retorna.push(buscarposicao($scope.posicao[index], item));
                 sons.push('src/objetotresmentrodireita.mp3');
             }
             else if ((item =>4) && (item < 5)) {
-                $scope.retorna.push($scope.posicao[index], item)
+                $scope.retorna.push(buscarposicao($scope.posicao[index], item));
                 sons.push('src/objetoquatromentrodireita.mp3');
             }
             else if ((item =>5) && (item < 6)) {
-                $scope.retorna.push($scope.posicao[index], item)
+                $scope.retorna.push(buscarposicao($scope.posicao[index], item));
                 sons.push('src/objetodoismentrodireita.mp3');
             }
             else if ((item =>6) && (item < 7)) {
-                $scope.retorna.push($scope.posicao[index], item)
+                $scope.retorna.push(buscarposicao($scope.posicao[index], item));
                 sons.push('src/objetotresmentrodireita.mp3');
             }
             else if ((item =>7) && (item < 8)) {
-                $scope.retorna.push($scope.posicao[index], item)
+                $scope.retorna.push(buscarposicao($scope.posicao[index], item));
                 sons.push('src/objetoquatromentrodireita.mp3');
             }
             else if ((item =>8) && (item < 9)) {
-                $scope.retorna.push($scope.posicao[index], item)
+                $scope.retorna.push(buscarposicao($scope.posicao[index], item));
+                sons.push('src/objetosmaisdezmetros.mp3');
+            }
+            else if (item == 0) {
+                $scope.retorna.push(buscarposicao($scope.posicao[index], item));
                 sons.push('src/objetosmaisdezmetros.mp3');
             };
-
 
 
 
